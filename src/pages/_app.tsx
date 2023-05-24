61e35744b6278fdb703442aee5b637ef65a3590e
do_xuan_trung_iMac <trung13988@gmail.com>
2023-05-24 08:49:24 +0700

    META TAG - change location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ApolloProvider } from "@apollo/client";
 import { SessionProvider } from "next-auth/react";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
@@ -100,6 +101,12 @@ function App({ Component, pageProps }: MattressAppProps) {
                         <InitComponent />
                         {/* @ts-ignore */}
                         <Layout>
+                            <Head>
+                                <meta
+                                    name="viewport"
+                                    content="width=device-width, initial-scale=1, maximum-scale=1"
+                                />
+                            </Head>
                             <Component {...pageProps} />
                         </Layout>
                     </AppStateContext.Provider>
