aba2110d95e6da9396620fad93f26286cd2f19bd
do_xuan_trung_iMac <trung13988@gmail.com>
2023-05-31 10:23:44 +0700

    INFORMATION PAGE - add more UI

--- src/container/home/HomePage.tsx
+++ src/container/home/HomePage.tsx
@@ -9,7 +9,6 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { Fragment } from "react";
 import * as Yup from "yup";
-import HomePageSliderBlock from "./content/HomePageSliderBlock";
 
 export interface IHomePageProps {
     [key: string]: any;
--- src/container/information/InformationPage.tsx
+++ src/container/information/InformationPage.tsx
@@ -1,53 +1,42 @@
 /* eslint-disable react/no-unescaped-entities */
 import Path from "@/common/constant/path";
-import SlickSlider from "@/components/slider/SlickSlider";
 import ViewShowMore from "@/components/view/ViewShowMore";
 import Messages from "@/languages/Messages";
 import styled from "@emotion/styled";
-import { Button, Icon, ViewTextarea } from "d-react-components";
+import { Button } from "d-react-components";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { Fragment } from "react";
-import * as Yup from "yup";
-import InformationSliderBlock from "./content/InformationSliderBlock";
+import BlockInformation from "../shared/block/BlockInformation";
+import BlockSwiperSlide from "../shared/block/BlockSwiperSlide";
 
 export interface IInformationPageProps {
     [key: string]: any;
 }
 
-const IMG_MOBILE = 250;
-const IMG_DESKTOP = 600;
-
-enum TypeOfData {
-    HIGH = "HIGH",
-    MEDIUM = "MEDIUM",
-    LOW = "LOW",
-}
-const HOME_PAGE_COVERS = [
+const BLOCK_INFORMATION = [
     {
-        id: TypeOfData.HIGH,
-        label: "High",
-        src: "/images/home-page-cover.jpeg",
+        id: "1",
+        title: Messages.introduction,
+        description:
+            "At Pirate Mobile, we understand the importance of staying connected wherever you go. Introducing Pirate Mobile eSim, your passport to hassle-free global connectivity. Say goodbye to physical SIM cards and unlock a world of seamless communication and data access right from your device.",
+        image: "/images/logo/logo.png",
     },
     {
-        id: TypeOfData.MEDIUM,
-        label: "Medium",
-        src: "/images/home-page-cover-1.jpeg",
+        id: "2",
+        title: Messages.whatIsPirateMobile,
+        description:
+            "Pirate Mobile eSim is a revolutionary technology that allows you to activate a mobile connection without the need for a physical SIM card. With a simple download and activation process, you can enjoy the benefits of a local mobile network wherever you travel. Say hello to convenience and flexibility like never before.",
+        image: "/images/logo/logo.png",
     },
     {
-        id: TypeOfData.LOW,
-        label: "Low",
-        src: "/images/home-page-cover-2.jpeg",
+        id: "3",
+        title: Messages.keyFeatures,
+        description:
+            " Global Coverage: Enjoy coverage in over 180 countries, ensuring you're always connected, no matter where your adventures take you. Easy Activation: Activate your eSim with a few simple steps, eliminating the need to visit a physical store or wait for a SIM card to be shipped to you. Dual SIM Support: Seamlessly switch between your eSim and physical SIM card for ultimate convenience and flexibility. Cost-Effective: Say goodbye to expensive international roaming charges. Pirate Mobile eSim offers affordable plans that help you stay connected without breaking the bank. Data Plans: Choose from a range of data plans tailored to your specific needs, whether you're a frequent traveler or an occasional explorer. No Contract Required: Enjoy the freedom of a no-contract commitment. Activate and deactivate your eSim as needed without any long-term obligations.",
+        image: "/images/logo/logo.png",
     },
 ];
-
-const validationSchema = Yup.object().shape({
-    dataType: Yup.string().required("Required field!"),
-    location: Yup.string().required("Required field!"),
-    email: Yup.string()
-        .email("Email must be a valid email!")
-        .required("Required field!"),
-});
 
 const InformationPage: React.FC<IInformationPageProps> = ({ id }) => {
     const router = useRouter();
@@ -94,29 +83,6 @@ const InformationPage: React.FC<IInformationPageProps> = ({ id }) => {
                     />
                 </div>
             </section>
-        );
-    };
-
-    const renderHomepageSlider = () => {
-        return (
-            <SlickSlider setting={{ autoplay: true, autoplaySpeed: 5000 }}>
-                {HOME_PAGE_COVERS.map((item) => {
-                    return (
-                        <div
-                            key={item?.id}
-                            className="home-page__slider-image-wrapper"
-                        >
-                            <Image
-                                alt="slider-homepage"
-                                src={item?.src}
-                                className="home-page__slider-image"
-                                layout="fill"
-                                style={{ objectFit: "cover" }}
-                            />
-                        </div>
-                    );
-                })}
-            </SlickSlider>
         );
     };
 
@@ -176,12 +142,29 @@ const InformationPage: React.FC<IInformationPageProps> = ({ id }) => {
         );
     };
 
+    const renderInformationBlocks = () => {
+        return (
+            <Fragment>
+                {BLOCK_INFORMATION.map((block, index) => {
+                    return (
+                        <BlockInformation
+                            key={block?.id}
+                            item={block}
+                            position={index % 2 === 0 ? "left" : "right"}
+                        />
+                    );
+                })}
+            </Fragment>
+        );
+    };
+
     return (
-        <MainStyled className="home-page__container container bg-transparent z-10 relative text-white px-3 ">
+        <MainStyled className="home-page__container bg-transparent z-10 relative text-white ">
             {renderHeader()}
             <div className="h-screen overflow-y-scroll hide-scroll-bar-y">
-                <InformationSliderBlock />
-                {/* {renderText()} */}
+                <BlockSwiperSlide />
+                <div className="px-3">{renderInformationBlocks()}</div>
+
                 <div className="h-52" />
             </div>
         </MainStyled>
