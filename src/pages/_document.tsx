131e924fb7c116e579efda0663ae8434db784923
do_xuan_trung_iMac <trung13988@gmail.com>
2023-06-01 16:55:14 +0700

    add SEO to the site

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -70,7 +70,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="shortcut icon" href="/images/favicon.ico" />
+                <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
