9705c4689b460d6791c8b15e7696fc90636e95bc
do_xuan_trung_iMac <trung13988@gmail.com>
2023-06-06 15:33:21 +0700

    add PAYPAL production config

--- src/configuration/ProConfig.ts
+++ src/configuration/ProConfig.ts
@@ -8,4 +8,8 @@ export default {
     GOOGLE_CLIENT_SECRET:
         process.env.GOOGLE_CLIENT_SECRET ||
         "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",
+    PAYPAL_CLIENT_ID:
+        "Ab6X_PfrObilAQyu7PG1NPJ87Pd5dJo3ArbJae-pnxWT6H7LmO4IohXuiyKE2GMmHf7KiXLXTdsHw7Q7",
+    PAYPAL_SECRET_CODE:
+        "EMPxdHQhEFV4IDodEJiTfL3GsnoiQdyaJI8tIJ8iKQjV0OSLZ4Ibargpf_UV6wzHp7u69GxmfTiesjYT",
 };
