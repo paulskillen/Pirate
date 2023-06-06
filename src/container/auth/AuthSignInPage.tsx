880ad8f013a605a497bdd4f3aebfb80df47730bd
do_xuan_trung_iMac <trung13988@gmail.com>
2023-06-06 18:02:14 +0700

    update sign in page

--- src/container/auth/AuthSignInPage.tsx
+++ src/container/auth/AuthSignInPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import AuthSignInView from "./shared/AuthSignInView";
 
 export interface IAuthSignInPageProps {
     [key: string]: any;
 }
 
 const AuthSignInPage: React.FC<IAuthSignInPageProps> = ({ id }) => {
-    return <div>AuthSignInPage</div>;
+    return <AuthSignInView />;
 };
 
 export default AuthSignInPage;
