be9fc721a9019bacace4fc4571dfe6e4ecd0194a
Trung DX <trung13988@gmail.com>
2023-06-10 14:32:19 +0700

    LOCATION - add region enum

---
+++ src/common/interface/location.ts
@@ -0,0 +1,10 @@
+export enum CountryRegion {
+    Middle_East = "Middle East",
+    Asia = "Asia",
+    Africa = "Africa",
+    Europe = "Europe",
+    Oceania = "Oceania",
+    South_America = "South America",
+    North_America = "North America",
+    Global = "Global",
+}
