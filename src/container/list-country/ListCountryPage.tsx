45ef2dafc16f7a6addb81bb30bb9408504735d53
Trung DX <trung13988@gmail.com>
2023-07-11 21:15:33 +0700

    refactor

--- src/container/list-country/ListCountryPage.tsx
+++ src/container/list-country/ListCountryPage.tsx
@@ -1,11 +1,11 @@
 import Path from "@/common/constant/path";
-import ClassNames from "classnames";
-import { Element, scroller, Link } from "react-scroll";
+import ClassNames from 'classnames';
 import { AppStateContext } from "@/common/context/app/app-context";
 import { ICountry } from "@/common/interface/location";
 import Image from "@/components/image/Image";
 import AppLink from "@/components/link/AppLink";
 import Messages from "@/languages/Messages";
+import styled from "@emotion/styled";
 import { Button, InputTextSearch } from "d-react-components";
 import { forEach, map } from "lodash";
 import { useRouter } from "next/router";
@@ -16,9 +16,8 @@ import React, {
     useMemo,
     useState,
 } from "react";
+import { Element, Link } from "react-scroll";
 import { useScroll, useSessionStorage } from "react-use";
-import styled from "@emotion/styled";
-import { COLOR_GOLD } from "@/common/constant/app-style";
 
 export interface IListCountryPageProps {
     [key: string]: any;
@@ -68,25 +67,6 @@ const ListCountryPage: React.FC<IListCountryPageProps> = ({ id }) => {
         });
         return found;
     }, []);
-
-    useEffect(() => {
-        const handleOnScroll = () => {
-            const list = listRef.current;
-            const childNodes = list?.childNodes;
-            // console.log({ y });
-
-            // console.log(
-            //     "🚀 >>>>>> file: ListCountryPage.tsx:73 >>>>>> handleOnScroll >>>>>> childs:",
-            //     childNodes
-            // );
-        };
-
-        const scrollElement = scrollRef?.current;
-        scrollElement &&
-            scrollElement?.addEventListener("scroll", handleOnScroll);
-        return () =>
-            scrollElement?.removeEventListener("scroll", handleOnScroll);
-    }, [listRef, y]);
 
     useEffect(() => {
         if (value && value > 0) {
